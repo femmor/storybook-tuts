6878234d0aba5f72981719ebbee8651d112aeb38
Emmanuel Egomson <emmanuelegomson@E-Egomson.attlocal.net>
2021-07-22 17:32:36 -0500

    create story within story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -10,6 +10,7 @@ export const Primary = () => <Button variant="primary">Primary</Button>
 export const Secondary = () => <Button variant="secondary">Secondary</Button>
 export const Success = () => <Button variant="success">Success</Button>
 export const Danger = () => <Button variant="danger">Danger</Button>
+export const Stretch = () => <Button variant='primary stretch' >Stretched Button</Button>
 export const Disabled = () => <Button variant="disabled" disabled>Disabled</Button>
 
 Disabled.storyName = "Disabled button"
---
+++ src/components/Subscription/Subscription.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Stretch} from "../Button/Button.stories"
+import {Large} from "../Input/Input.stories"
+
+
+
+export default {
+  title: 'Components/Subscription Form'
+}
+
+export const PrimarySubscription = () => {
+  return(
+    <>
+      <Large />
+      <Stretch />
+    </>
+  )
+}
